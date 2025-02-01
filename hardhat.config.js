54b05a4deb5a593b423ec78b290b39100ff9ddc9
Catin Hat <109822733+nasta11@users.noreply.github.com>
2025-02-01 20:55:16 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,16 @@
-require('@nomiclabs/hardhat-ethers');
-require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.27",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
